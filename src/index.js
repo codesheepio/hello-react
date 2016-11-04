571503ae7b6ece027c0bb45fdb7e483d5220c9e8
Arnupharp Viratanapanu <Arnupharp Viratanapanu>
2016-11-04 15:43:05 +0700

    Add stylesheet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Counter from './components/Counter'
-
+import './style.css'
 
 ReactDOM.render(
   <Counter label="I'm a counter" />,
