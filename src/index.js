69fae7d68bb206ce1ef692a3c777d1d7957bcbf1
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-03-14 23:03:02 +0700

    Migrate to webpack2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom'
 import Counter from './components/Counter'
 import './style.css'
 
-ReactDOM.render(
-  <Counter label="I'm a counter" />,
-  document.getElementById('react-root')
+const render = (Counter) => (
+  ReactDOM.render(
+    <Counter label="I'm a counter" />,
+    document.getElementById('react-root')
+  )
 )
+render(Counter)
+if (module.hot) {
+  module.hot.accept('./components/Counter', () => {
+    render(Counter)
+  })
+}
+
 
